7263cbff3fc5f4f61984db507be287abaa0d179e
Jessica White <jessica2714white@gmail.com>
2021-06-22 22:27:54 -0500

    Moved arrays from local to global variables

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ var userPassword = "";
 // Variable index is open-ended which helps with the randomness necessary for particular assignment
 var index;
 
+// arrays 
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!", "@", "#", "$", "&", "*", "^"];
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
@@ -20,7 +26,31 @@ function writePassword() {
 function generatePassword() {
     var preferences = getChoices();
     console.log(preferences);
+
+    console.log(userOptions);
+    while (userPassword.length <= length) {
+        if (preferences.useUppercase) {
+            index = indexRandom(uppercase.length);
+            userPassword = userPassword + uppercase[index];
+        }
+        if (prefererences.useLowercase) {
+            index = indexRandom(lowercase.length);
+            userPassword = userPassword + lowercase[index];
+        }
+        if (preferences.useNumeric) {
+            index = indexRandom(numeric.length);
+            userPassword = userPassword + numeric[index];
+        }
+        if (preferences.useSpec) {
+            index = indexRandom(special.length);
+            userPassword = userPassword + special[index];
+        }
+    }
+console.log("hello", userPassword); 
+
+return userPassword;
 }
+
 
 //write function that generates possible choices
 function getChoices() {
@@ -35,10 +65,6 @@ function getChoices() {
         alert("Not acceptable");
     }
 
-    var useUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var useLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var useNumeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    var useSpec = ["!", "@", "#", "$", "&", "*", "^"];
 
     // Utilize confirm prompt to have user choose if they want to use certain characters or not
     var useUppercase = confirm("Click okay if you want to use uppercase letters");
@@ -65,35 +91,6 @@ function getChoices() {
     return (userOptions);
 }
 
-// validate user input ... at least 1 of those has to be true for conditional
-// best option is to use a while loop
-
-function coolPass(userOptions) {
-    console.log(userOptions);
-    while (userPassword.length <= length) {
-        if (useUppercase) {
-            index = indexRandom(uppercase.length);
-            userPassword = userPassword + uppercase[index];
-        }
-        if (useLowercase) {
-            index = indexRandom(lowercase.length);
-            userPassword = userPassword + lowercase[index];
-        }
-        if (useNumeric) {
-            index = indexRandom(numeric.length);
-            userPassword = userPassword + numeric[index];
-        }
-        if (useSpec) {
-            index = indexRandom(special.length);
-            userPassword = userPassword + special[index];
-        }
-    }
-console.log(userPassword);
-
-return userPassword;
-}
-
-
 // build password of specified number of characters (Rich's example)
 // for(let x = 0; x < numCharacters; x++) {
 // let randomCharacter = availableCharacters[Math.floor(Math.random() * availableCharacters.length)];
@@ -102,6 +99,7 @@ return userPassword;
 // }
 // return password; 
 
+// my version... 
 // for(let i = 0; i < length; i++) {
 //     let ____ = userOptions[Math.floor(Math.random() * userOptions.length)];
 //     console.log("________", ____);
